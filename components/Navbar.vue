2ac840ee26e4f1e3d73dacf6e44c459c03211529
raya <rayavexed@gmail.com>
2022-01-17 18:44:14 +0700

    color

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,56 +1,59 @@
 <template>
   <header
-    :class="navColor"
-    class="flex flex-col lg:flex-row z-40 items-center shadow-lg text-xl lg:text-base justify-between lg:justify-center font-medium backdrop-blur-2xl transition duration-500 h-12 lg:h-16 fixed px-5 w-full"
+    class="flex bg-white z-40 bg-opacity-70 items-center justify-center shadow-sm text-xl lg:text-base font-medium backdrop-blur-sm transition duration-500 h-12 lg:h-16 fixed w-full"
   >
-    <nav
-      :class="!open ? 'hidden' : 'bg-white'"
-      class="lg:flex flex flex-col items-center justify-center absolute lg:static w-full lg:w-auto h-screen lg:h-auto transition duration-500"
-    >
-      <ul class="flex flex-col lg:flex-row items-center justify-center">
-        <li
-          :class="textColor"
-          class="px-3 py-3 lg:py-0"
-          v-for="(l, idx) in link"
-          :key="idx"
-          @click="active(l.to)"
+    <div class="flex flex-col lg:flex-row-reverse w-11/12 lg:w-10/12 ">
+      <nav
+        :class="!open ? 'hidden' : 'bg-white'"
+        class="lg:block flex flex-col items-center justify-center absolute lg:static w-full h-screen lg:h-auto transition duration-500"
+      >
+        <ul
+          class="flex flex-col lg:flex-row lg:space-x-2 items-center justify-center text-center lg:justify-end"
         >
-          <rhc-link
-            :textColor="textColor"
-            :textHover="textHover"
-            :activeColor="activeColor"
-            :active="l.to == isActive"
-            :to="l.to"
-          >{{l.section}}</rhc-link>
-        </li>
-      </ul>
-    </nav>
-    <div class="flex items-center h-full justify-between w-full lg:hidden">
-      <div :class="textColor" class="lg:hidden">RHC</div>
-      <button @click="menuClick" :class="textColor" class="block lg:hidden z-40">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            v-if="!open"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M4 6h16M4 12h16m-7 6h7"
-          />
-          <path
-            v-else
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M6 18L18 6M6 6l12 12"
-          />
-        </svg>
-      </button>
+          <li
+            :class="textColor"
+            class="px-3 py-3 lg:py-0"
+            v-for="(l, idx) in link"
+            :key="idx"
+            @click="active(l.to)"
+          >
+            <rhc-link
+              :textColor="textColor"
+              :textHover="textHover"
+              :activeColor="activeColor"
+              :active="l.to == isActive"
+              :to="l.to"
+            >{{l.section}}</rhc-link>
+          </li>
+        </ul>
+      </nav>
+      <div class="flex items-center h-full justify-between w-full lg:w-auto">
+        <div :class="textColor">Brand</div>
+        <button @click="menuClick" :class="textColor" class="block lg:hidden z-40">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              v-if="!open"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M4 6h16M4 12h16m-7 6h7"
+            />
+            <path
+              v-else
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </button>
+      </div>
     </div>
   </header>
 </template>
@@ -69,18 +72,18 @@ export default {
         { to: '#site', section: 'Site' },
         { to: '#contacts', section: 'Contacs' },
       ],
-      textColor: 'text-white text-opacity-50',
-      navColor: '',
+      textColor: 'text-gray-500',
+      navColor: 'bg-white',
       navColorSm: '',
-      activeColor: 'text-white',
-      textHover: 'text-white',
+      activeColor: 'text-indigo-600',
+      textHover: 'text-indigo-600',
       isActive: '#hero',
       scrollPosition: null,
       open: false,
     }
   },
   mounted() {
-    window.addEventListener('scroll', this.updateScroll)
+    // window.addEventListener('scroll', this.updateScroll)
   },
   methods: {
     active(to) {
@@ -89,23 +92,27 @@ export default {
     },
     menuClick() {
       this.open = !this.open
-      this.textColor = 'text-gray-400'
-      this.textHover = 'text-blue-700'
+      this.textColor = 'text-gray-500'
+      this.textHover = 'text-indigo-600'
       this.navColorSm = 'bg-white'
-      this.activeColor = 'text-blue-700'
+      this.activeColor = 'text-indigo-600'
     },
     updateScroll() {
       this.scrollPosition = window.scrollY
       if (this.scrollPosition < 100) {
-        this.textColor = 'text-gray-400 lg:text-white lg:text-opacity-50'
-        this.textHover = 'text-blue-700 lg;text-white'
-        this.navColor = 'bg-transparent'
-        this.activeColor = 'text-blue-700 lg:text-white'
-      } else {
-        this.textColor = 'text-gray-400'
-        this.textHover = 'text-blue-700'
+        // this.textColor = 'text-gray-400 lg:text-white lg:text-opacity-50'
+        // this.textHover = 'text-indigo-600 lg;text-white'
+        // this.navColor = 'bg-transparent'
+        // this.activeColor = 'text-indigo-600 lg:text-white'
+        this.textColor = 'text-gray-500'
+        this.textHover = 'text-indigo-600'
         this.navColor = 'bg-white'
-        this.activeColor = 'text-blue-700'
+        this.activeColor = 'text-indigo-600'
+      } else {
+        this.textColor = 'text-gray-500'
+        this.textHover = 'text-indigo-600'
+        this.navColor = 'bg-white'
+        this.activeColor = 'text-indigo-600'
       }
     },
   },
--- components/Sectionss/Site.vue
+++ components/Sectionss/Site.vue
@@ -1,24 +1,24 @@
 <template>
   <div id="site" class="h-auto flex flex-col items-center justify-center">
     <Container class="my-16 lg:my-24">
-      <rhc-title class="text-blue-700">Site - Property</rhc-title>
+      <rhc-title class="text-indigo-600">Site - Property</rhc-title>
       <div class="min-h-96 mt-12 lg:mt-4 lg:px-10 w-full flex flex-col lg:flex-row justify-between">
         <div class="w-full lg:w-7/12 text-center lg:text-left">
           <div class="mb-10">
-            <h1 class="text-blue-800 text-xl font-semibold mb-4">Site Area</h1>
+            <h1 class="text-indigo-600 text-xl font-semibold mb-4">Site Area</h1>
             <span class="text-gray-500 text-md">54,410 sqm</span>
           </div>
           <div class="mb-10">
-            <h1 class="text-blue-800 text-xl font-semibold mb-4">Legal Description</h1>
+            <h1 class="text-indigo-600 text-xl font-semibold mb-4">Legal Description</h1>
             <span class="text-gray-500 text-md">15 (fifteen) Right to Build (HGB) Certificates</span>
           </div>
           <div class="mb-10">
-            <h1 class="text-blue-800 text-xl font-semibold mb-4">Town Planning</h1>
+            <h1 class="text-indigo-600 text-xl font-semibold mb-4">Town Planning</h1>
             <table class="table-auto ">
               <tbody>
                 <tr v-for="(tp,idx) in townPlanning" :key="idx">
-                  <td class="px-2 pb-3 text-right lg:text-left">{{tp.desc}}</td>
-                  <td class="px-2 pb-3 text-left font-semibold">{{tp.value}}</td>
+                  <td class="pr-2 pb-3 text-right lg:text-left">{{tp.desc}}</td>
+                  <td class="pr-2 pb-3 text-left font-semibold">{{tp.value}}</td>
                 </tr>
               </tbody>
             </table>
@@ -30,7 +30,7 @@
             alt="Site Plan"
             class="h-80 hover:scale-105 transition duration-300"
           />
-          <h1 class="text-blue-800 text-xl font-semibold mb-4">Lot Plan</h1>
+          <h1 class="text-indigo-600 text-xl font-semibold mb-4">Lot Plan</h1>
         </div>
       </div>
     </Container>
