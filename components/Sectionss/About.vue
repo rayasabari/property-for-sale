bea4201a1e271a8de6b276d0cc6ba6d31c88329c
raya <rayavexed@gmail.com>
2022-01-19 18:14:57 +0700

    mobile view

--- components/Sectionss/About.vue
+++ components/Sectionss/About.vue
@@ -2,10 +2,10 @@
   <section id="about" class="flex items-center justify-center">
     <Container class="my-16 lg:my-20">
       <rhc-title class="text-orange-400">About Us</rhc-title>
-      <div class="flex flex-col lg:flex-row justify-center space-y-8 lg:space-y-0 lg:space-x-8 text-md leading-loose text-justify">
+      <div class="flex flex-col lg:flex-row justify-center space-y-8 lg:space-y-0 lg:space-x-8 text-md leading-loose text-center lg:text-justify">
         <div class="w-full lg:w-6/12 p-4">
           <div class="w-full flex justify-center items-center">
-            <img src="@/assets/images/logo-rhr-black.png" alt="Colliers Logo" class="h-20 mb-10" />
+            <img src="@/assets/images/logo-rhr-black.png" alt="Colliers Logo" class="h-20 mb-8 lg:mb-10" />
           </div>
           <p
             class="mb-2"
@@ -21,7 +21,7 @@
         </div>
         <div class="w-full lg:w-6/12 p-4">
           <div class="w-full flex justify-center items-center">
-            <img src="@/assets/images/logo-colliers.png" alt="Colliers Logo" class="h-20 mb-10" />
+            <img src="@/assets/images/logo-colliers.png" alt="Colliers Logo" class="h-20 mb-8 lg:mb-10" />
           </div>
           <p>Colliers International is the leader in global real estate services, defined by our spirit of enterprise. Through a culture of service excellence and a shared sense of initiative, we integrate the resources of real estate specialists worldwide to accelerate the success of our partners. Colliers International delivers real estate services globally through 15,000 specialized professionals in more than 396 offices in 68 countries. Colliers provides advice to clients on more than USD105 billion in transactions annually and manages 2 billion square feet with revenue exceeding USD2.6 billion.</p>
           <a class="font-medium text-orange-500" href="https://www.colliers.com">www.colliers.com</a>
