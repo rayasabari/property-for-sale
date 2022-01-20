f41b4e3c7e5c222f9be8646e33cdb84936998e3a
raya <rayavexed@gmail.com>
2022-01-20 13:08:14 +0700

    fix img

--- components/Partials/RhcFloating.vue
+++ components/Partials/RhcFloating.vue
@@ -4,7 +4,7 @@
     href="#"
     class="h-14 w-14 shadow-sm bottom-7 right-7 rounded-full bg-green-500 hover:bg-green-600 transition duration-500 fixed z-50 flex justify-center items-center"
   >
-    <font-awesome-icon class="text-4xl text-white" :icon="['fab', 'whatsapp']" />
+    &nbsp;<font-awesome-icon class="text-4xl text-white" :icon="['fab', 'whatsapp']" />&nbsp;
   </a>
 </template>
 
