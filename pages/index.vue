740a506098befe9f37a0a7dca272a8559af556da
raya <rayavexed@gmail.com>
2022-01-20 19:50:11 +0700

    ads

--- pages/index.vue
+++ pages/index.vue
@@ -34,3 +34,13 @@ export default {
   },
 }
 </script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-217719492-1">
+</script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-217719492-1');
+</script>
